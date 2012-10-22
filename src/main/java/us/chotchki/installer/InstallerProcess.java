51a9414b26256b96cf2bb641ef3cf4021ef8b19c
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-10-22 19:15:03 -0400

    Reworked the installer process to support non web.xml startups.

--- src/main/java/us/chotchki/installer/InstallerProcess.java
+++ src/main/java/us/chotchki/installer/InstallerProcess.java
@@ -1,9 +1,6 @@
 package us.chotchki.installer;
 
-import us.chotchki.installer.classpath.ScanningLoader;
-
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.naming.Context;
@@ -15,48 +12,50 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import us.chotchki.installer.classpath.ScanningLoader;
+
 public class InstallerProcess implements ServletContextListener {
 	private static Logger log = LoggerFactory.getLogger(InstallerProcess.class);
 
 	@Override
-	public void contextDestroyed(ServletContextEvent sce) {}
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		String installerJndiPath = sce.getServletContext().getInitParameter("us.chotchki.installer.InstallerProcess.jndi");
-		if(installerJndiPath == null) {
+		String installerJndiPath = sce.getServletContext().getInitParameter(
+				"us.chotchki.installer.InstallerProcess.jndi");
+		if (installerJndiPath == null) {
 			log.error("Unable to get the context parameter for installer.");
 			return;
 		}
-		
-		Connection conn = null;
-		try {
-		   Context initCtx = new InitialContext();
-		   Context envCtx = (Context) initCtx.lookup("java:comp/env");
-		   DataSource ds = (DataSource) envCtx.lookup(installerJndiPath);
-		   if(ds == null) {
-			   throw new Exception("Lookup failed for the installer using the path " + installerJndiPath);
-		   }
-		   conn = ds.getConnection();
-		   if(conn == null) {
-			   throw new Exception("Unable to establish a database connection");
-		   }
-		   conn.setAutoCommit(false);
-		} catch(Exception e) {
-			log.error("Failed try to connect to the database.", e);
-			if(conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e1) {}
-			}
-		}
-		
-		try {
-			List<Part> installParts = ScanningLoader.createParts(conn);
 
-			for(Part p: installParts) {
-				if(p.isInstalled()) {
-					log.info("Part {} is already installed", p.getClass().getName());
+		try {
+			install(installerJndiPath);
+		} catch(Exception e){
+			log.error("Installation Process Failed!", e);
+		}
+	}
+
+	public void install(String jndiPath) throws Exception {
+		Context initCtx = new InitialContext();
+		Context envCtx = (Context) initCtx.lookup("java:comp/env");
+		DataSource ds = (DataSource) envCtx.lookup(jndiPath);
+		if (ds == null) {
+			throw new Exception("Lookup failed for the installer using the path " + jndiPath);
+		}
+		try (Connection conn = ds.getConnection()){
+			if (conn == null) {
+				throw new Exception("Unable to establish a database connection");
+			}
+			conn.setAutoCommit(false);
+			
+			List<Part> installParts = ScanningLoader.createParts(conn);	
+
+			for (Part p : installParts) {
+				if (p.isInstalled()) {
+					log.info("Part {} is already installed", p.getClass()
+							.getName());
 				} else {
 					log.info("Installing Part {}", p.getClass().getName());
 					p.install();
@@ -64,17 +63,9 @@ public class InstallerProcess implements ServletContextListener {
 			}
 			conn.commit();
 			log.info("Installation Complete!");
-			
+
 			InstallerFlag.INSTANCE.enabled.set(false);
 			log.info("Disabled the install filter");
-		} catch (Exception e) {
-			log.error("Installation process failed", e);
-		} finally {
-			if(conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e1) {}
-			}
 		}
 	}
 }
