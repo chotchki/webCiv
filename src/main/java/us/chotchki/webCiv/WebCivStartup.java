cb525966f39a616da0638cd3105deff9b00d1875
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-10-24 19:00:09 -0400

    Starting testing the start of the webapp

--- src/main/java/us/chotchki/webCiv/WebCivStartup.java
+++ src/main/java/us/chotchki/webCiv/WebCivStartup.java
@@ -1,11 +1,19 @@
 package us.chotchki.webCiv;
 
+import java.util.Set;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import us.chotchki.installer.InstallerProcess;
 
@@ -13,9 +21,9 @@ public class WebCivStartup implements WebApplicationInitializer {
 	private static final Logger log = LoggerFactory.getLogger(WebCivStartup.class);
 
 	@Override
-	public void onStartup(ServletContext arg0) throws ServletException {
+	public void onStartup(ServletContext sc) throws ServletException {
 		log.info("Starting up WebCiv!");
-		
+
 		log.info("Installing database schema.");
 		InstallerProcess ip = new InstallerProcess();
 		try {
@@ -23,6 +31,25 @@ public class WebCivStartup implements WebApplicationInitializer {
 		} catch (Exception e) {
 			log.error("Install process failed", e);
 			throw new ServletException(e);
+		}
+
+		AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+		root.scan("us.chotchki.webCiv");
+		root.getEnvironment().setDefaultProfiles("embedded");
+
+		sc.addListener(new ContextLoaderListener(root));
+
+		// Secures the application
+		sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false, "/*");
+
+		// Handles requests into the application
+		ServletRegistration.Dynamic appServlet = sc.addServlet("appServlet", new DispatcherServlet(new GenericWebApplicationContext()));
+		appServlet.setLoadOnStartup(1);
+		Set<String> mappingConflicts = appServlet.addMapping("/");
+		if (!mappingConflicts.isEmpty()) {
+			throw new IllegalStateException("'appServlet' could not be mapped to '/' due "
+					+ "to an existing mapping. This is a known issue under Tomcat versions "
+					+ "<= 7.0.14; see https://issues.apache.org/bugzilla/show_bug.cgi?id=51278");
 		}
 	}
 
