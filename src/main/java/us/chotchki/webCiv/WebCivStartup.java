ce075888b0037b7348b03e46f449e220c5007256
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-10-24 19:40:07 -0400

    Added a skeleton sitemesh config.

--- src/main/java/us/chotchki/webCiv/WebCivStartup.java
+++ src/main/java/us/chotchki/webCiv/WebCivStartup.java
@@ -1,11 +1,14 @@
 package us.chotchki.webCiv;
 
+import java.util.EnumSet;
 import java.util.Set;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import javax.servlet.SessionTrackingMode;
 
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
@@ -48,6 +51,9 @@ public class WebCivStartup implements WebApplicationInitializer {
 		// Secures the application
 		//sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false, "/*");
 
+		//Sitemesh Setup
+		sc.addFilter("sitemesh", ConfigurableSiteMeshFilter.class).addMappingForUrlPatterns(null, false, "/*");
+		
 		// Handles requests into the application
 		ServletRegistration.Dynamic appServlet = sc.addServlet("appServlet", new DispatcherServlet(new GenericWebApplicationContext()));
 		appServlet.setLoadOnStartup(1);
@@ -57,6 +63,9 @@ public class WebCivStartup implements WebApplicationInitializer {
 					+ "to an existing mapping. This is a known issue under Tomcat versions "
 					+ "<= 7.0.14; see https://issues.apache.org/bugzilla/show_bug.cgi?id=51278");
 		}
+		
+		//Force cookie use for sessions
+		sc.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
 	}
 
 }
---
+++ src/main/java/us/chotchki/webCiv/web/Index.java
@@ -0,0 +1,13 @@
+package us.chotchki.webCiv.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
