e403fc16388257126e45177ccd87e52df3a4c8b5
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-11-04 20:22:15 -0500

    adding null checks

--- src/main/java/us/chotchki/webCiv/config/WebCivStartup.java
+++ src/main/java/us/chotchki/webCiv/config/WebCivStartup.java
@@ -45,10 +45,12 @@ public class WebCivStartup implements WebApplicationInitializer {
 		
 		//Force UTF-8
 		FilterRegistration.Dynamic charEncodingfilterReg = sc.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
-		charEncodingfilterReg.setInitParameter("encoding", "UTF-8");
-		charEncodingfilterReg.setInitParameter("forceEncoding", "true");
-		charEncodingfilterReg.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
-
+		if(charEncodingfilterReg != null){
+			charEncodingfilterReg.setInitParameter("encoding", "UTF-8");
+			charEncodingfilterReg.setInitParameter("forceEncoding", "true");
+			charEncodingfilterReg.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
+		}
+		
 		// Secures the application
 		//sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false, "/*");
 
