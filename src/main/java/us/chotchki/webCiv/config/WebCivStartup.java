f39e768f00af81a8f3adb77fe2d941a6492d9df7
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-11-04 21:26:44 -0500

    I commented the entire method!

--- src/main/java/us/chotchki/webCiv/config/WebCivStartup.java
+++ src/main/java/us/chotchki/webCiv/config/WebCivStartup.java
@@ -23,22 +23,22 @@ public class WebCivStartup implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext sc) throws ServletException {
-		log.info("Starting up WebCiv!");
-
-		log.info("Installing database schema.");
-		InstallerProcess ip = new InstallerProcess();
-		try {
-			ip.install("jdbc/webCiv");
-		} catch (Exception e) {
-			log.error("Install process failed", e);
-			throw new ServletException(e);
-		}
-
-		AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
-		root.scan("us.chotchki.webCiv");
-
-		sc.addListener(new ContextLoaderListener(root));
-		
+		//log.info("Starting up WebCiv!");
+//
+	//	log.info("Installing database schema.");
+		//InstallerProcess ip = new InstallerProcess();
+		//try {
+		//	ip.install("jdbc/webCiv");
+		//} catch (Exception e) {
+		//	log.error("Install process failed", e);
+		//	throw new ServletException(e);
+		//}
+//
+		//AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+		//root.scan("us.chotchki.webCiv");
+//
+		//sc.addListener(new ContextLoaderListener(root));
+		//
 		//Force UTF-8
 		//FilterRegistration.Dynamic charEncodingfilterReg = sc.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
 		//if(charEncodingfilterReg != null){
@@ -46,31 +46,31 @@ public class WebCivStartup implements WebApplicationInitializer {
 		//	charEncodingfilterReg.setInitParameter("forceEncoding", "true");
 		//	charEncodingfilterReg.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
 		//}
-		
+		//
 		// Secures the application
 		//sc.addFilter("securityFilter", new DelegatingFilterProxy("springSecurityFilterChain")).addMappingForUrlPatterns(null, false, "/*");
-
+//
 		//Sitemesh Setup
 		//FilterRegistration.Dynamic sitemeshReg = sc.addFilter("sitemesh3", ConfigurableSiteMeshFilter.class);
 		//if(sitemeshReg != null){
 		//	sitemeshReg.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/*");
 		//}
-		
-		
+	//	
+		//
 		// Handles requests into the application
-		ServletRegistration.Dynamic appServlet = sc.addServlet("appServlet", new DispatcherServlet(new GenericWebApplicationContext()));
-		if(appServlet != null){
-			appServlet.setLoadOnStartup(1);
-			Set<String> mappingConflicts = appServlet.addMapping("/w");
-			if (!mappingConflicts.isEmpty()) {
-				throw new IllegalStateException("'appServlet' could not be mapped to '/' due "
-						+ "to an existing mapping. This is a known issue under Tomcat versions "
-						+ "<= 7.0.14; see https://issues.apache.org/bugzilla/show_bug.cgi?id=51278");
-			}
-		}
-		
+		//ServletRegistration.Dynamic appServlet = sc.addServlet("appServlet", new DispatcherServlet(new GenericWebApplicationContext()));
+		//if(appServlet != null){
+			//appServlet.setLoadOnStartup(1);
+			//Set<String> mappingConflicts = appServlet.addMapping("/w");
+			//if (!mappingConflicts.isEmpty()) {
+				//throw new IllegalStateException("'appServlet' could not be mapped to '/' due "
+					//	+ "to an existing mapping. This is a known issue under Tomcat versions "
+					//	+ "<= 7.0.14; see https://issues.apache.org/bugzilla/show_bug.cgi?id=51278");
+			//}
+		//}
+		//
 		//Force cookie use for sessions
-		sc.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+		//sc.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
 	}
 
 }
