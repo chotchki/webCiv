9caa17779e0064352e46dcbebfd97336c071273f
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2012-12-28 14:05:15 -0500

    Missed an annotation on UserDao

--- src/main/java/us/chotchki/webCiv/db/dao/UserDao.java
+++ src/main/java/us/chotchki/webCiv/db/dao/UserDao.java
@@ -1,7 +1,10 @@
 package us.chotchki.webCiv.db.dao;
 
+import org.springframework.stereotype.Repository;
+
 import us.chotchki.webCiv.db.pojo.User;
 
+@Repository
 public class UserDao extends ParentDao {
 	public void create(User u){
 		getSession().save(u);
